64f6765b033a647ad55df2d8f3433a77ac666b2c
ClintLedbetter <clint.ledbetter@csuglobal.edu>
2026-02-17 21:27:06 -0700

    Initial commit – CSC450 Module 01 projects

---
+++ CSC450_Mod01_PersonInfo/PersonInfo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string firstName = "Michael";
+    std::string lastName  = "Jordan";
+    std::string street    = "23 Bulls Street";
+    std::string city      = "Chicago, IL";
+    std::string zip       = "12345";
+
+    std::cout << "First Name: " << firstName << '\n';
+    std::cout << "Last Name: " << lastName << '\n';
+    std::cout << "Street Address: " << street << '\n';
+    std::cout << "City: " << city << '\n';
+    std::cout << "Zip Code: " << zip << '\n';
+
+    return 0;
+}
